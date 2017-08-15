251a9dcb5256c96db59e5e6eee50e1750ae29845
Александр Шилов <aleksandr.schilow2012@gmail.com>
2017-08-15 18:57:21 +0300

    Методы для проверки clustering-ключа и limit'ов

--- src/acapella/kv/utils/assertion.py
+++ src/acapella/kv/utils/assertion.py
@@ -1,9 +1,16 @@
-from typing import List
+from typing import List, Optional
 
 
 def check_key(key: List[str]):
     if len(key) == 0:
         raise ValueError("key must not be empty")
+    for part in key:
+        check_key_part(part)
+
+
+def check_clustering(key: Optional[List[str]]):
+    if key is None:
+        return
     for part in key:
         check_key_part(part)
 
@@ -26,3 +33,10 @@ def check_nrw(n: int, r: int, w: int):
         raise ValueError("R parameter must be less then or equal to N")
     if w > n:
         raise ValueError("W parameter must be less then or equal to N")
+
+
+def check_limit(limit: Optional[int]):
+    if limit is None:
+        return
+    if limit < 0:
+        raise ValueError("limit parameter must be greater than or equal to zero")
