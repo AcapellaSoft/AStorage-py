cc4cddbdddc947cc2660128f57fc74ec4dc21732
Александр Шилов <aleksandr.schilow2012@gmail.com>
2017-08-21 13:06:29 +0300

    Тперь не нужно оборачивать batch-методы в ensure_future

--- src/acapella/kv/Entry.py
+++ src/acapella/kv/Entry.py
@@ -1,5 +1,7 @@
 from datetime import timedelta
-from typing import List, Optional
+from typing import List, Optional, Awaitable, Callable
+
+import asyncio
 
 from acapella.kv.BatchBase import BatchBase
 from acapella.kv.utils.assertion import check_key, check_nrw
@@ -81,7 +83,7 @@ class Entry(object):
         self._value = body.get('value')
         return self._value
 
-    async def set(self, new_value: Optional[object], batch: Optional[BatchBase] = None) -> int:
+    def set(self, new_value: Optional[object], batch: Optional[BatchBase] = None) -> Awaitable[int]:
         """
         Устанавливает новое значение.
         Запоминает новые значение и версию.        
@@ -94,27 +96,29 @@ class Entry(object):
         :raise TransactionCompletedError: когда транзакция, в которой выполняется операция, уже завершена
         :raise KvError: когда произошла неизвестная ошибка на сервере
         """
-        if batch is None:
-            url = entry_url(self._partition, self._clustering)
-            response = await self._session.put(url, params={
-                'n': self._n,
-                'r': self._r,
-                'w': self._w,
-                'transaction': self._transaction,
-            }, json=new_value)
-            raise_if_error(response.status_code)
+        return single_or_batch(batch, self._set_single, self._set_batch, new_value)
 
-            body = response.json()
-            self._version = int(body['version'])
-            self._value = new_value
-            return self._version
-        else:
-            self._version = await batch.set(self.partition, self.clustering, new_value, self._n, self._r, self._w)
-            self._value = new_value
-            return self._version
+    async def _set_batch(self, batch: BatchBase, new_value: Optional[object]):
+        self._version = await batch.set(self.partition, self.clustering, new_value, self._n, self._r, self._w)
+        self._value = new_value
+        return self._version
 
-    async def cas(self, new_value: Optional[object], old_version: Optional[int] = None,
-                  batch: Optional[BatchBase] = None) -> int:
+    async def _set_single(self, new_value: Optional[object]):
+        url = entry_url(self._partition, self._clustering)
+        response = await self._session.put(url, params={
+            'n': self._n,
+            'r': self._r,
+            'w': self._w,
+            'transaction': self._transaction,
+        }, json=new_value)
+        raise_if_error(response.status_code)
+        body = response.json()
+        self._version = int(body['version'])
+        self._value = new_value
+        return self._version
+
+    def cas(self, new_value: Optional[object], old_version: Optional[int] = None,
+                  batch: Optional[BatchBase] = None) -> Awaitable[int]:
         """
         Устанавливает новое значение при совпадении версий.
         
@@ -130,27 +134,29 @@ class Entry(object):
         """
         if old_version is None:
             old_version = self._version
+        return single_or_batch(batch, self._cas_single, self._cas_batch, new_value, old_version)
 
-        if batch is None:
-            url = entry_url(self._partition, self._clustering)
-            response = await self._session.put(url, params={
-                'n': self._n,
-                'r': self._r,
-                'w': self._w,
-                'transaction': self._transaction,
-                'oldVersion': old_version,
-            }, json=new_value)
-            raise_if_error(response.status_code)
+    async def _cas_batch(self, batch: BatchBase, new_value: Optional[object], old_version: int):
+        self._version = await batch.cas(
+            self.partition, self.clustering, new_value, old_version, self._n, self._r, self._w
+        )
+        self._value = new_value
+        return self._version
 
-            body = response.json()
-            self._version = int(body['version'])
-            self._value = new_value
-            return self._version
-        else:
-            self._version = await batch.cas(self.partition, self.clustering, new_value, old_version,
-                                            self._n, self._r, self._w)
-            self._value = new_value
-            return self._version
+    async def _cas_single(self, new_value: Optional[object], old_version: int):
+        url = entry_url(self._partition, self._clustering)
+        response = await self._session.put(url, params={
+            'n': self._n,
+            'r': self._r,
+            'w': self._w,
+            'transaction': self._transaction,
+            'oldVersion': old_version,
+        }, json=new_value)
+        raise_if_error(response.status_code)
+        body = response.json()
+        self._version = int(body['version'])
+        self._value = new_value
+        return self._version
 
     @property
     def value(self) -> Optional[object]:
@@ -179,3 +185,10 @@ class Entry(object):
         :return: сортируемый ключ
         """
         return self._clustering
+
+
+def single_or_batch(batch: Optional[BatchBase], fn_single: Callable, fn_batch: Callable, *args) -> Awaitable:
+    if batch is None:
+        return fn_single(*args)
+    else:
+        return asyncio.ensure_future(fn_batch(batch, *args))
--- tests/test.py
+++ tests/test.py
@@ -383,10 +383,6 @@ class TestDtRange(TestCase):
         assert result[1].value == 'bar'
 
 
-def _async(f):
-    asyncio.ensure_future(f)
-
-
 class TestKvBatch(TestCase):
     @async_test
     async def test_batch_set(self):
@@ -394,11 +390,11 @@ class TestKvBatch(TestCase):
         p1 = random_key()
         p2 = random_key()
 
-        _async(session.entry(p1, ['aaa']).set('111', batch))
-        _async(session.entry(p1, ['bbb']).set('222', batch))
-        _async(session.entry(p2, ['aaa']).set('333', batch))
-        _async(session.entry(p2, ['bbb']).set('444', batch))
-        _async(session.entry(p2, ['ccc']).set('555', batch))
+        session.entry(p1, ['aaa']).set('111', batch)
+        session.entry(p1, ['bbb']).set('222', batch)
+        session.entry(p2, ['aaa']).set('333', batch)
+        session.entry(p2, ['bbb']).set('444', batch)
+        session.entry(p2, ['ccc']).set('555', batch)
 
         await batch.send()
 
@@ -407,6 +403,26 @@ class TestKvBatch(TestCase):
         assert '333' == (await session.get_entry(p2, ['aaa'])).value
         assert '444' == (await session.get_entry(p2, ['bbb'])).value
         assert '555' == (await session.get_entry(p2, ['ccc'])).value
+
+    @async_test
+    async def test_batch_set_awaitable(self):
+        batch = session.batch_manual()
+        p1 = random_key()
+
+        f = session.entry(p1, ['aaa']).set('111', batch)
+        await batch.send()
+
+        asyncio.wait_for(1.0, f)
+
+    @async_test
+    async def test_batch_cas(self):
+        batch = session.batch_manual()
+        p1 = random_key()
+
+        f = session.entry(p1, ['aaa']).cas('111', 0, batch)
+        await batch.send()
+
+        assert '111' == (await session.get_entry(p1, ['aaa'])).value
 
 
 if __name__ == '__main__':
